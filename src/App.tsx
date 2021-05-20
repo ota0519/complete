21d55fd65375b6e9afce43c25a4df71e9add9358
ota0519 <70121865+ota0519@users.noreply.github.com>
2021-05-20 14:59:01 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+
+import { Router } from "./router/Router";
+
+import theme from "./theme/theme";
+
+export const App = () => {
+  return (
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ChakraProvider>
+  );
+};
